dd4d2f855f6888ef59b9cba0d07fdddb10fb48c0
suraj <kambojsuraj255@gmail.com>
2021-07-03 20:36:33 +0530

    First Commit

---
+++ src/test/java/QAClickAcademy/MavenJava/RestAPITest.java
@@ -0,0 +1,18 @@
+package QAClickAcademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void PostJiraTest()
+	{
+		System.out.println("PostJiraTest");
+	}
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+}
---
+++ src/test/java/QAClickAcademy/MavenJava/SeleniumTest.java
@@ -0,0 +1,18 @@
+package QAClickAcademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementsUI()
+	{
+		System.out.println("ElementUI");
+	}
+}
